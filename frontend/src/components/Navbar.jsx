7d3f64937b171db59cd5af3b69e4056d12e660c2
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-11-28 19:40:33 +0600

    navbar search button is completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/feather.png';
+
 const Navbar = () => {
-    return <h1>Navbar</h1>;
+    return (
+        <nav className="navbar">
+            <Link>
+                <img src={logo} alt="" className="flex-none w-10 h-8 mr-[5vw]" />
+            </Link>
+
+            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-gray py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+                <input type="text" placeholder="Search" className="w-full md:w-auto bg-gray p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-gray  md:pl-12" />
+
+                <i className="fi fi-rr-search absolute right-[10%] top-1/2 -translate-y-1/2 md:pointer-events-none md:left-5 text-xl text-dark-gray"></i>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
