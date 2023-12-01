dc3e11306cda4521e7c49fa2694e937ace382a47
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-12-01 11:16:56 +0600

    some routes are set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const App = () => {
-    return <Navbar />;
+    return (
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="signin" element={<h1>Sign in</h1>} />
+                <Route path="signup" element={<h1>Sign up</h1>} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
