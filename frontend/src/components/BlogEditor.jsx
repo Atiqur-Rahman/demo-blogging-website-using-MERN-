702ba33c8e233b8c6acba8f3c07989fe9fb185a3
Atiqur-Rahman <rahmanatique88@gmail.com>
2024-01-06 21:36:33 +0600

    BlogEditor Banner is completed

--- frontend/src/components/BlogEditor.jsx
+++ frontend/src/components/BlogEditor.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/feather.png';
+import AnimationWrapper from '../common/AnimationWrapper';
+import defaultBanner from '../assets/blog banner.png';
 
 const BlogEditor = () => {
+    const handleBannerUpload = (e) => {
+        let img = e.target.files[0];
+        console.log(img);
+    };
+
     return (
         <>
             <nav className="navbar">
@@ -16,6 +23,19 @@ const BlogEditor = () => {
                     <button className="btn-light py-2">Save Draft</button>
                 </div>
             </nav>
+
+            <AnimationWrapper>
+                <section>
+                    <div className="mx-auto max-w-[900px] w-full">
+                        <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-gray">
+                            <label htmlFor="uploadBanner" className="cursor-pointer">
+                                <img src={defaultBanner} alt="" />
+                                <input id="uploadBanner" type="file" accept=".png, .jpg, .jpeg" hidden onChange={handleBannerUpload} />
+                            </label>
+                        </div>
+                    </div>
+                </section>
+            </AnimationWrapper>
         </>
     );
 };
