c952b98acfc5c38e6d91ca2ed81ec59ffabef984
Atiqur-Rahman <rahmanatique88@gmail.com>
2024-01-06 20:21:03 +0600

    BlogEditor component's navbar is completed

--- frontend/src/components/BlogEditor.jsx
+++ frontend/src/components/BlogEditor.jsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/feather.png';
+
 const BlogEditor = () => {
-    return <div>This Blog Editor</div>;
+    return (
+        <>
+            <nav className="navbar">
+                <Link to="/">
+                    <img src={logo} className="flex-none w-14 h-12" alt="" />
+                </Link>
+
+                <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+
+                <div className="flex gap-4 ml-auto">
+                    <button className="btn-dark py-2">Publish</button>
+                    <button className="btn-light py-2">Save Draft</button>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default BlogEditor;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,10 +28,10 @@ const Navbar = () => {
         <>
             <nav className="navbar">
                 <Link>
-                    <img src={logo} alt="" className="flex-none w-10 h-8 mr-[5vw]" />
+                    <img src={logo} alt="" className="flex-none w-14 h-12" />
                 </Link>
 
-                <div className={`absolute bg-white w-full left-0 top-full mt-0.5 border-b border-gray py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${searchBoxVisibility ? 'show' : 'hide'}`}>
+                <div className={`absolute bg-white w-full left-0 top-full ml-[4vw] mt-0.5 border-b border-gray py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${searchBoxVisibility ? 'show' : 'hide'}`}>
                     <input type="text" placeholder="Search" className="w-full md:w-auto bg-gray p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-gray md:pl-12" />
 
                     <i className="fi fi-rr-search absolute right-[10%] top-1/2 -translate-y-1/2 md:pointer-events-none md:left-5 text-xl text-dark-gray"></i>
