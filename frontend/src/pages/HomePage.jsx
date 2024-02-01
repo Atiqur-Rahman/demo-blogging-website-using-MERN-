f8da075e025cd19f8ab8b2aaa43c9a2e8ed6847e
Atiqur-Rahman <rahmanatique88@gmail.com>
2024-02-01 22:35:33 +0600

    HomePage - fatching latest blogs is done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,40 @@
+import axios from 'axios';
 import AnimationWrapper from '../common/AnimationWrapper';
 import InPageNavigation from '../components/InPageNavigation';
+import { useEffect, useState } from 'react';
+import Loader from '../components/Loader';
 
 const HomePage = () => {
+    const [blogs, setBlogs] = useState();
+
+    const fetchLatestBlogs = () => {
+        axios
+            .get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
+            .then(({ data }) => {
+                setBlogs(data.blogs);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchLatestBlogs();
+    }, []);
+
     return (
         <AnimationWrapper>
             <section className="h-cover flex justify-center gap-10">
                 {/* latest blogs  */}
                 <div className="w-full">
                     <InPageNavigation routes={['home', 'trending blog']} defaultHidden={['trending blog']}>
-                        <h1>Latest Blogs Here</h1>
+                        {blogs == null ? (
+                            <Loader />
+                        ) : (
+                            blogs.map((blog, i) => {
+                                return <h1 key={i}>{blog.title}</h1>;
+                            })
+                        )}
 
                         <h1>Trending Blogs Here</h1>
                     </InPageNavigation>
